d121592533d7685ad16efd88dec1dc2ac43ff44b
Adeleye Olamide <adeleyeolamide56@gmail.com>
2019-08-03 16:57:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/travelmantics/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.travelmantics;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_travel_deal, menu);
+        return true;
+    }
+}
