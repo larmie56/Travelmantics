344a03099372ab8730a5d6f70ea73d6ddcc38fed
Adeleye Olamide <adeleyeolamide56@gmail.com>
2019-08-04 08:08:23 +0100

    Updated version, to include writing to Firebase database

--- app/src/main/java/com/example/travelmantics/MainActivity.java
+++ app/src/main/java/com/example/travelmantics/MainActivity.java
@@ -4,13 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mDatabaseReference;
+    private EditText mDealTitle;
+    private EditText mDealPrice;
+    private EditText mDealDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mDatabase.getReference().child("travelDeals");
+
+        mDealTitle = findViewById(R.id.editText_travel_deal_title);
+        mDealPrice = findViewById(R.id.editText_travel_deal_price);
+        mDealDescription = findViewById(R.id.editText_travel_deal_description);
+
+
     }
 
     @Override
@@ -18,5 +39,36 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.save_travel_deal, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_save_travel_deal:
+                saveDeal();
+                Toast.makeText(this, "Deal saved!", Toast.LENGTH_LONG).show();
+                clean();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void clean() {
+        mDealTitle.setText("");
+        mDealPrice.setText("");
+        mDealDescription.setText("");
+    }
+
+    private void saveDeal() {
+        String title = mDealTitle.getText().toString();
+        String price = mDealPrice.getText().toString();
+        String imageUrl = "";
+
+        TravelDeal deal = new TravelDeal(title, price, imageUrl);
+
+        mDatabaseReference.push().setValue(deal);
     }
 }
---
+++ app/src/main/java/com/example/travelmantics/TravelDeal.java
@@ -0,0 +1,49 @@
+package com.example.travelmantics;
+
+class TravelDeal {
+
+    private String id;
+    private String title;
+    private String price;
+    private String imageUrl;
+
+    public TravelDeal(String title, String price, String imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+}
